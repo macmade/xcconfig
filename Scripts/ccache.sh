2ba35feb399c214c66f984017529058eccd6e403
macmade <macmade@xs-labs.com>
2018-01-15 21:19:36 +0100

    Support for distcc

--- Scripts/ccache.sh
+++ Scripts/ccache.sh
@@ -13,7 +13,17 @@ else
 fi
 
 if type -p ccache > /dev/null 2>&1; then
+    
+    if test -f ~/.distcc/hosts && type -p distcc > /dev/null 2>&1; then
         
+        export CCACHE_PREFIX="distcc"
+        
+    elif test -f ~/.distcc/hosts && type -p /usr/local/distcc/bin/distcc > /dev/null 2>&1; then
+        
+        export CCACHE_PREFIX="/usr/local/distcc/bin/distcc"
+        
+    fi
+    
     exec ccache $CLANG "$@"
     
 else
